95f250fd4d4bd052bb847b1cd68e465f6a8c683e
Feba Christina Joel <febachristinaj31@gmail.com>
2022-12-14 17:58:26 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angularproject';
+  check = false;
+
+  tableArray = [
+    {
+      productName: 'News Mobile App...',
+      status: 'Active',
+      sold: '10',
+      view: '100',
+    },
+    {
+      productName: '40+ Landing Page Template...',
+      status: 'Active',
+      sold: '10',
+      view: '100',
+    },
+    {
+      productName: 'SmartTVDoctor consultant UI KIT...',
+      status: 'Active',
+      sold: '10',
+      view: '100',
+    },
+    {
+      productName: 'Startup Illlustration KIT Ve...',
+      status: 'Active',
+      sold: '10',
+      view: '100',
+    }
+  ];
+
+  searchform() {
+    this.check = true;
+    console.log('hi');
+  }
+
+  submitForm(item: any) {
+    console.log(this.check);    
+    this.tableArray.push(item);
+    // console.log(this.tableArray);
+    
+    this.cancel();
+  }
+
+  cancel() {
+    this.check = false;
+  }
+
+  // delete item from the table
+  del(index: any) {
+    console.log(index);
+    this.tableArray.splice(index, 1);
+    // delete this.tableArray[i];
+    console.log(this.tableArray);
+  }
 }
